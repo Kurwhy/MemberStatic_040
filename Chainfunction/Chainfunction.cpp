3b78bc1b4b46214028ac8005bda71d408b4d3a6f
Wahyu <firmansyahw503@gmail.com>
2023-05-17 17:28:32 +0700

    final

---
+++ Chainfunction/Chainfunction.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class buku
+{
+	string judul;
+
+public:
+	buku setJudul(string Judul)
+	{
+		this->judul = judul;
+		return *this; //chain function
+	}
+	string getJudul()
+	{
+		return this->judul;
+	}
+} bukunya;
+
+int main()
+{
+	//bukunya.setJudul("Matematika");
+	// cout << bukunya.getJudul();
+	cout << bukunya.setJudul("matematika").getJudul(); // chain function calls
+	return 0;
+}
